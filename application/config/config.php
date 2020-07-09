28893dc817bf6fa987ccb6fa7bf3b9c556f56ad3
mv3.edvan <edvan.pereira@mv3.com.br>
2020-07-09 17:38:27 -0300

    Alteracao base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Gestao_Oficinas/blabla';
+$config['base_url'] = 'http://localhost/Gestao_Oficina/';
 
 /*
 |--------------------------------------------------------------------------
